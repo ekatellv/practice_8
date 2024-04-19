f44e94ecb282bf790f92e5df4e53e27f53bab7ff
ekatellv <159680448+ekatellv@users.noreply.github.com>
2024-04-19 21:14:59 +0700

    Create 9_ex.py

---
+++ 9_ex.py
@@ -0,0 +1,11 @@
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+N = int(input())
+for i in range(2, N+1):
+    if is_prime(i):
+        print(i)
